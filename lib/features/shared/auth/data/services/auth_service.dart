ef4c93bc17f788a4d878ae3491b6a2025edffe83
Igor Sthanny <igorsthaynny@gmail.com>
2025-02-21 10:40:25 -0300

    feat: create auth service

---
+++ lib/features/shared/auth/data/services/auth_service.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logging/logging.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  final _log = Logger('AuthService');
+
+  // Sign up with email and password
+  Future<User?> signUpWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      _log.warning('Error signing up: $e');
+      return null;
+    }
+  }
+
+  // Sign in with email and password
+  Future<User?> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      _log.warning('Error signing in: $e');
+      return null;
+    }
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      _log.warning('Error signing out: $e');
+    }
+  }
+
+  // Get current user
+  User? getCurrentUser() {
+    try {
+      return _firebaseAuth.currentUser;
+    } catch (e) {
+      _log.warning('Error getting current user: $e');
+      return null;
+    }
+  }
+}
