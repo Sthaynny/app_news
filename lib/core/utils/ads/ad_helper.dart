88a26f42d2aa8649fa811ad378855cfa43a3b027
Igor Sthanny <igorsthaynny@gmail.com>
2025-04-23 09:53:48 -0300

    feat: create mapper ad base

---
+++ lib/core/utils/ads/ad_helper.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+abstract base class AdHelper {
+  static String get bannerAd {
+    if (kDebugMode) return _bannerTest;
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-1899836354814268/4387620929';
+    } else if (Platform.isIOS) {
+      return '';
+    } else {
+      return '';
+    }
+  }
+
+  static String get interstitialAd {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-1899836354814268/1055165983';
+    } else if (Platform.isIOS) {
+      return '';
+    } else {
+      return '';
+    }
+  }
+
+  static String get _bannerTest => 'ca-app-pub-3940256099942544/6300978111';
+}
